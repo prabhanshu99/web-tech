959c5591f7d48863437c94cf4e44ae7dee9878d2
Prabhanshu <prabhanshuop@gmail.com>
2026-03-28 06:17:19 +0530

    added 10th

---
+++ react/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+      <p>This is my first React component.</p>
+    </div>
+  )
+}
+
+export default App
